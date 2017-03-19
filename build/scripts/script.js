36dbb5da739d6606eead928517b2feeae5d54aed
Allan Ortiz <aortiz@g2llc.com>
2017-03-18 20:11:07 -0700

    conditional polyfill usage; center text

--- build/scripts/script.js
+++ build/scripts/script.js
@@ -1342,7 +1342,24 @@ function getTouchPos(canvasDom, touchEvent) {
   };
 }
 
+function polyfillIfNecessary() {
+  if ('registerElement' in document
+      && 'import' in document.createElement('link')
+      && 'content' in document.createElement('template')) {
+    // platform is good!
+  } else {
+    // polyfill the platform!
+    var e = document.createElement('script');
+    e.src = '/bower_components/webcomponentsjs/webcomponents-lite.min.js';
+    document.body.appendChild(e);
+  }
+}
+
 function init() {
+
+  polyfillIfNecessary();
+  
+  
   let FPS = 60;
 
   world = new World();
